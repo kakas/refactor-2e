9c6af37f8f689090f40606103f6a1e7d833614b6
kakas <shrimptrain@gmail.com>
2020-02-16 16:55:25 +0800

    整理 volumeCredits

--- chapter_1/createStatementData.js
+++ chapter_1/createStatementData.js
@@ -9,12 +9,8 @@ class PerformanceCalculator {
   }
 
   get volumeCredits() {
-    let result = 0
-    result += Math.max(this.performance.audience - 30, 0)
-    if (this.play.type === 'comedy') result += Math.floor(this.performance.audience / 5)
-    return result
+    return Math.max(this.performance.audience - 30, 0)
   }
-
 }
 
 class TragedyCalculator extends PerformanceCalculator {
@@ -43,6 +39,10 @@ class ComedyCalculator extends PerformanceCalculator {
     }
     result += 300 * this.performance.audience
     return result
+  }
+
+  get volumeCredits() {
+    return super.volumeCredits + Math.floor(this.performance.audience / 5)
   }
 }
 
