f91bcff3e501e4dfb3d7a58c362023d7b73a3882
kakas <shrimptrain@gmail.com>
2020-02-16 16:24:12 +0800

    Add createStatementData simple test

--- chapter_1/statement.test.js
+++ chapter_1/statement.test.js
@@ -1,4 +1,5 @@
 const { statement } = require('./statement')
+const createStatementData = require('./createStatementData')
 
 const plays = {
   hamlet: { name: 'Hamlet', type: 'tragedy' },
@@ -23,6 +24,23 @@ const invoice = {
     }
   ]
 }
+
+describe('createStatementData', () => {
+  test('basic test', () => {
+    const expectedResult = {
+      customer: "BigCo",
+      performances: [
+        { amount: 65000, audience: 55, play: { name: "Hamlet", type: "tragedy" }, playID: "hamlet", volumeCredits: 25 },
+        { amount: 58000, audience: 35, play: { name: "As You Like It", type: "comedy" }, playID: "as-like", volumeCredits: 12 },
+        { amount: 50000, audience: 40, play: { name: "Othello", type: "tragedy" }, playID: "othello", volumeCredits: 10 },
+      ],
+      totalAmount: 173000,
+      totalVolumeCredits: 47,
+    }
+
+    expect(createStatementData(invoice, plays)).toStrictEqual(expectedResult)
+  })
+})
 
 describe('statement', () => {
   test('basic test', () => {
