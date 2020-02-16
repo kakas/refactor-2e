a2b3c0b35943611ef3963488999dddb8b4fc9779
kakas <shrimptrain@gmail.com>
2020-02-16 13:22:35 +0800

    Refactor: move function

--- chapter_1.js
+++ chapter_1.js
@@ -1,27 +1,3 @@
-function amountFor(aPerformance, play) {
-  let result = 0
-
-  switch (play.type) {
-  case 'tragedy':
-    result = 40000
-    if (aPerformance.audience > 30) {
-      result += 1000 * (aPerformance.audience - 30)
-    }
-    break
-  case 'comedy':
-    result = 30000
-    if (aPerformance.audience > 20) {
-      result += 10000 + (500 * (aPerformance.audience - 20))
-    }
-    result += 300 * aPerformance.audience
-    break
-  default:
-    throw new Error(`unknown type: ${play.type}`)
-  }
-
-  return result
-}
-
 function statement(invoice, plays) {
   let totalAmount = 0
   let volumeCredits = 0
@@ -31,6 +7,30 @@ function statement(invoice, plays) {
     currency: 'USD',
     minimumFractionDigits: 2,
   }).format
+
+  function amountFor(aPerformance, play) {
+    let result = 0
+
+    switch (play.type) {
+    case 'tragedy':
+      result = 40000
+      if (aPerformance.audience > 30) {
+        result += 1000 * (aPerformance.audience - 30)
+      }
+      break
+    case 'comedy':
+      result = 30000
+      if (aPerformance.audience > 20) {
+        result += 10000 + (500 * (aPerformance.audience - 20))
+      }
+      result += 300 * aPerformance.audience
+      break
+    default:
+      throw new Error(`unknown type: ${play.type}`)
+    }
+
+    return result
+  }
 
   function playFor(aPerformance) {
     return plays[aPerformance.playID]
