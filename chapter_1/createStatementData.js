70c3aa41ecc78496999ba8c37fd924ec8158338a
kakas <shrimptrain@gmail.com>
2020-02-16 16:35:05 +0800

    將 amountFor 移到 calculator

--- chapter_1/createStatementData.js
+++ chapter_1/createStatementData.js
@@ -1,3 +1,33 @@
+class PerformanceCalculator {
+  constructor(aPerformance, play) {
+    this.performance = aPerformance
+    this.play = play
+  }
+
+  get amount() {
+    let result = 0
+
+    switch (this.play.type) {
+    case 'tragedy':
+      result = 40000
+      if (this.performance.audience > 30) {
+        result += 1000 * (this.performance.audience - 30)
+      }
+      break
+    case 'comedy':
+      result = 30000
+      if (this.performance.audience > 20) {
+        result += 10000 + (500 * (this.performance.audience - 20))
+      }
+      result += 300 * this.performance.audience
+      break
+    default:
+      throw new Error(`unknown type: ${this.play.type}`)
+    }
+    return result
+  }
+}
+
 function createStatementData(invoice, plays) {
   const statementData = {}
   statementData.customer = invoice.customer
@@ -7,37 +37,15 @@ function createStatementData(invoice, plays) {
 
   function enrichPerformance(aPerformance) {
     const result = Object.assign({}, aPerformance)
-    result.play = playFor(result)
-    result.amount = amountFor(result)
+    const calculator = new PerformanceCalculator(aPerformance, playFor(result))
+    result.play = calculator.play
+    result.amount = calculator.amount
     result.volumeCredits = volumeCreditsFor(result)
     return result
   }
 
   function playFor(aPerformance) {
     return plays[aPerformance.playID]
-  }
-
-  function amountFor(aPerformance) {
-    let result = 0
-
-    switch (aPerformance.play.type) {
-    case 'tragedy':
-      result = 40000
-      if (aPerformance.audience > 30) {
-        result += 1000 * (aPerformance.audience - 30)
-      }
-      break
-    case 'comedy':
-      result = 30000
-      if (aPerformance.audience > 20) {
-        result += 10000 + (500 * (aPerformance.audience - 20))
-      }
-      result += 300 * aPerformance.audience
-      break
-    default:
-      throw new Error(`unknown type: ${aPerformance.play.type}`)
-    }
-    return result
   }
 
   function volumeCreditsFor(aPerformance) {
