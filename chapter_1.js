2c2f684c1fbecf2a7d1ed734591879288c196e6f
kakas <shrimptrain@gmail.com>
2020-02-16 13:40:42 +0800

    Refactor extract function format

--- chapter_1.js
+++ chapter_1.js
@@ -2,11 +2,14 @@ function statement(invoice, plays) {
   let totalAmount = 0
   let volumeCredits = 0
   let result = `Statement for ${invoice.customer}\n`
-  const format = new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD',
-    minimumFractionDigits: 2,
-  }).format
+
+  function format(aNumber) {
+    return new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+      minimumFractionDigits: 2,
+    }).format(aNumber)
+  }
 
   function playFor(aPerformance) {
     return plays[aPerformance.playID]
