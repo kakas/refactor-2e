5e420611de2004afbd6cf782656632662ee9e09c
kakas <shrimptrain@gmail.com>
2020-02-16 14:31:17 +0800

    處理 volumeCredits

--- chapter_1.js
+++ chapter_1.js
@@ -7,6 +7,7 @@ function statement(invoice, plays) {
     const result = Object.assign({}, aPerformance)
     result.play = playFor(result)
     result.amount = amountFor(result)
+    result.volumeCredits = volumeCreditsFor(result)
     return result
   }
 
@@ -38,6 +39,14 @@ function statement(invoice, plays) {
     return result
   }
 
+  function volumeCreditsFor(aPerformance) {
+    let result = 0
+    result += Math.max(aPerformance.audience - 30, 0)
+    if (aPerformance.play.type === 'comedy') result += Math.floor(aPerformance.audience / 5)
+    return result
+  }
+
+
   return renderPlainText(statementData)
 }
 
@@ -53,17 +62,10 @@ function renderPlainText(data) {
     }).format(aNumber / 100)
   }
 
-  function volumeCreditsFor(aPerformance) {
-    let result = 0
-    result += Math.max(aPerformance.audience - 30, 0)
-    if (aPerformance.play.type === 'comedy') result += Math.floor(aPerformance.audience / 5)
-    return result
-  }
-
   function totalVolumeCredits() {
     let result = 0
     for (const perf of data.performances) {
-      result += volumeCreditsFor(perf)
+      result += perf.volumeCredits
     }
 
     return result
