90c6e1addc95744d44e50dfe11ef367af2e763d5
kakas <shrimptrain@gmail.com>
2020-02-16 13:29:12 +0800

    Refactor: change function declaration

--- chapter_1.js
+++ chapter_1.js
@@ -8,10 +8,14 @@ function statement(invoice, plays) {
     minimumFractionDigits: 2,
   }).format
 
-  function amountFor(aPerformance, play) {
+  function playFor(aPerformance) {
+    return plays[aPerformance.playID]
+  }
+
+  function amountFor(aPerformance) {
     let result = 0
 
-    switch (play.type) {
+    switch (playFor(aPerformance).type) {
     case 'tragedy':
       result = 40000
       if (aPerformance.audience > 30) {
@@ -26,18 +30,14 @@ function statement(invoice, plays) {
       result += 300 * aPerformance.audience
       break
     default:
-      throw new Error(`unknown type: ${play.type}`)
+      throw new Error(`unknown type: ${playFor(aPerformance).type}`)
     }
 
     return result
   }
 
-  function playFor(aPerformance) {
-    return plays[aPerformance.playID]
-  }
-
   for (const perf of invoice.performances) {
-    let thisAmount = amountFor(perf, playFor(perf))
+    let thisAmount = amountFor(perf)
 
     // 加入 volume credit
     volumeCredits += Math.max(perf.audience - 30, 0)
