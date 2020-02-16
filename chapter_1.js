edb962f4115656c5c2b93dd8ac3f243bd13ac4e4
kakas <shrimptrain@gmail.com>
2020-02-16 13:42:40 +0800

    Change function declaration

--- chapter_1.js
+++ chapter_1.js
@@ -3,12 +3,12 @@ function statement(invoice, plays) {
   let volumeCredits = 0
   let result = `Statement for ${invoice.customer}\n`
 
-  function format(aNumber) {
+  function usd(aNumber) {
     return new Intl.NumberFormat('en-US', {
       style: 'currency',
       currency: 'USD',
       minimumFractionDigits: 2,
-    }).format(aNumber)
+    }).format(aNumber / 100)
   }
 
   function playFor(aPerformance) {
@@ -50,11 +50,11 @@ function statement(invoice, plays) {
     volumeCredits += volumeCreditsFor(perf)
 
     // 印出這筆訂單
-    result += `${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${perf.audience} seats)\n`
+    result += `${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`
     totalAmount += amountFor(perf)
   }
 
-  result += `Amount owed is ${format(totalAmount / 100)}\n`
+  result += `Amount owed is ${usd(totalAmount)}\n`
   result += `You earned ${volumeCredits} credits\n`
 
   return result
