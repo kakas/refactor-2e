2556dd82e14f915a280d6e206340286af765ebb6
kakas <shrimptrain@gmail.com>
2020-02-16 14:40:03 +0800

    簡化程式

--- chapter_1.js
+++ chapter_1.js
@@ -48,19 +48,11 @@ function statement(invoice, plays) {
   }
 
   function totalAmount(data) {
-    let result = 0
-    for (const perf of data.performances) {
-      result += perf.amount
-    }
-    return result
+    return data.performances.reduce((total, p) => total + p.amount, 0)
   }
 
   function totalVolumeCredits(data) {
-    let result = 0
-    for (const perf of data.performances) {
-      result += perf.volumeCredits
-    }
-    return result
+    return data.performances.reduce((total, p) => total + p.volumeCredits, 0)
   }
 
   return renderPlainText(statementData)
