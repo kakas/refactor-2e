63ea78e2570f88872784d8891843aba9db26fd5b
kakas <shrimptrain@gmail.com>
2020-02-16 16:36:42 +0800

    將 volumeCredits 移到 calculator

--- chapter_1/createStatementData.js
+++ chapter_1/createStatementData.js
@@ -26,6 +26,14 @@ class PerformanceCalculator {
     }
     return result
   }
+
+  get volumeCredits() {
+    let result = 0
+    result += Math.max(this.performance.audience - 30, 0)
+    if (this.play.type === 'comedy') result += Math.floor(this.performance.audience / 5)
+    return result
+  }
+
 }
 
 function createStatementData(invoice, plays) {
@@ -40,19 +48,12 @@ function createStatementData(invoice, plays) {
     const calculator = new PerformanceCalculator(aPerformance, playFor(result))
     result.play = calculator.play
     result.amount = calculator.amount
-    result.volumeCredits = volumeCreditsFor(result)
+    result.volumeCredits = calculator.volumeCredits
     return result
   }
 
   function playFor(aPerformance) {
     return plays[aPerformance.playID]
-  }
-
-  function volumeCreditsFor(aPerformance) {
-    let result = 0
-    result += Math.max(aPerformance.audience - 30, 0)
-    if (aPerformance.play.type === 'comedy') result += Math.floor(aPerformance.audience / 5)
-    return result
   }
 
   function totalAmount(data) {
