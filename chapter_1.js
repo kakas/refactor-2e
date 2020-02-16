4a49afb279f3615cad18938b7300d6e96d4b6297
kakas <shrimptrain@gmail.com>
2020-02-16 13:10:10 +0800

    Refactor: extract function amountFor

--- chapter_1.js
+++ chapter_1.js
@@ -1,3 +1,27 @@
+function amountFor(perf, play) {
+  let thisAmount = 0
+
+  switch (play.type) {
+  case 'tragedy':
+    thisAmount = 40000
+    if (perf.audience > 30) {
+      thisAmount += 1000 * (perf.audience - 30)
+    }
+    break
+  case 'comedy':
+    thisAmount = 30000
+    if (perf.audience > 20) {
+      thisAmount += 10000 + (500 * (perf.audience - 20))
+    }
+    thisAmount += 300 * perf.audience
+    break
+  default:
+    throw new Error(`unknown type: ${play.type}`)
+  }
+
+  return thisAmount
+}
+
 function statement(invoice, plays) {
   let totalAmount = 0
   let volumeCredits = 0
@@ -10,25 +34,7 @@ function statement(invoice, plays) {
 
   for (const perf of invoice.performances) {
     const play = plays[perf.playID]
-    let thisAmount = 0
-
-    switch (play.type) {
-    case 'tragedy':
-      thisAmount = 40000
-      if (perf.audience > 30) {
-        thisAmount += 1000 * (perf.audience - 30)
-      }
-      break
-    case 'comedy':
-      thisAmount = 30000
-      if (perf.audience > 20) {
-        thisAmount += 10000 + (500 * (perf.audience - 20))
-      }
-      thisAmount += 300 * perf.audience
-      break
-    default:
-      throw new Error(`unknown type: ${play.type}`)
-    }
+    let thisAmount = amountFor(perf, play)
 
     // 加入 volume credit
     volumeCredits += Math.max(perf.audience - 30, 0)
