cdb2c73addc5b6935d6def0d150dc84dab02dcd3
kakas <shrimptrain@gmail.com>
2020-04-16 00:16:21 +0800

    extract function 7

--- chapter_6/1_extract_function.js
+++ chapter_6/1_extract_function.js
@@ -1,9 +1,8 @@
 function printOwing(invoice) {
-  let outstanding = 0;
-
   printBanner()
 
   // calculate outstanding
+  let outstanding = 0;
   for (const o of invoice.orders) {
     outstanding += o.amount;
   }
