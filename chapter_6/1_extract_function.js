9d0edd1fd5ec32c9aa1b67ef988a451ed0b5aa99
kakas <shrimptrain@gmail.com>
2020-04-16 00:10:13 +0800

    extract function 4

--- chapter_6/1_extract_function.js
+++ chapter_6/1_extract_function.js
@@ -12,12 +12,12 @@ function printOwing(invoice) {
   const today = Clock.today;
   invoice.dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30);
 
-  printDetails();
-  function printDetails() {
-    console.log(`name: ${invoice.customer}`);
-    console.log(`amount: ${outstanding}`);
-    console.log(`due: ${invoice.dueDate.toLocaleDateString()}`);
-  }
+  printDetails(invoice, outstanding);
+}
+function printDetails(invoice, outstanding) {
+  console.log(`name: ${invoice.customer}`);
+  console.log(`amount: ${outstanding}`);
+  console.log(`due: ${invoice.dueDate.toLocaleDateString()}`);
 }
 function printBanner() {
   console.log("***********************");
