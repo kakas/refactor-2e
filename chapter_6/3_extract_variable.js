9363e5d29b8ca28ea4b8d842f004c729bd08dbfe
kakas <shrimptrain@gmail.com>
2020-04-16 00:40:35 +0800

    extract variable 3

--- chapter_6/3_extract_variable.js
+++ chapter_6/3_extract_variable.js
@@ -1,7 +1,8 @@
 function price(order) {
   //price is base price - quantity discount + shipping
   const basePrice = order.quantity * order.itemPrice;
+  const quantityDiscount = Math.max(0, order.quantity - 500) * order.itemPrice * 0.05;
   return basePrice -
-    Math.max(0, order.quantity - 500) * order.itemPrice * 0.05 +
+    quantityDiscount +
     Math.min(basePrice * 0.1, 100);
 }
