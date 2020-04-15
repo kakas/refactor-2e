fed589e2c33e038b35a68aa4cafb31bfa211a8c6
kakas <shrimptrain@gmail.com>
2020-04-16 00:06:05 +0800

    extract function 2

--- chapter_6/1_extract_function.js
+++ chapter_6/1_extract_function.js
@@ -1,9 +1,7 @@
 function printOwing(invoice) {
   let outstanding = 0;
 
-  console.log("***********************");
-  console.log("**** Customer Owes ****");
-  console.log("***********************");
+  printBanner()
 
   // calculate outstanding
   for (const o of invoice.orders) {
@@ -18,4 +16,9 @@ function printOwing(invoice) {
   console.log(`name: ${invoice.customer}`);
   console.log(`amount: ${outstanding}`);
   console.log(`due: ${invoice.dueDate.toLocaleDateString()}`);
+}
+function printBanner() {
+  console.log("***********************");
+  console.log("**** Customer Owes ****");
+  console.log("***********************");
 }
