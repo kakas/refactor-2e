eddec2263344b31dca8a10f3a96b5bda108aaec8
kakas <shrimptrain@gmail.com>
2020-04-16 00:41:49 +0800

    extract variable 5

--- chapter_6/3_extract_variable.js
+++ chapter_6/3_extract_variable.js
@@ -5,3 +5,18 @@ function price(order) {
   const shipping = Math.min(basePrice * 0.1, 100);
   return basePrice - quantityDiscount + shipping
 }
+
+class Order {
+  constructor(aRecord) {
+    this._data = aRecord;
+  }
+
+  get quantity()  {return this._data.quantity;}
+  get itemPrice() {return this._data.itemPrice;}
+
+  get price() {
+    return this.quantity * this.itemPrice -
+      Math.max(0, this.quantity - 500) * this.itemPrice * 0.05 +
+      Math.min(this.quantity * this.itemPrice * 0.1, 100);
+  }
+}
