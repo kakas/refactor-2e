9e513f61ad7f330c272e164911616b825a9fac54
kakas <shrimptrain@gmail.com>
2020-04-16 00:34:46 +0800

    inline function 4

--- chapter_6/2_inline_function.js
+++ chapter_6/2_inline_function.js
@@ -2,9 +2,5 @@ function reportLines(aCustomer) {
   const lines = [];
   out.push(["name", aCustomer.name]);
   out.push(["location", aCustomer.location]);
-  gatherCustomerData(lines, aCustomer);
   return lines;
-}
-
-function gatherCustomerData(out, aCustomer) {
 }
