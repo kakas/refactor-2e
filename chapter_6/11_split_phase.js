30dd38d61e6901965675a5082e3693515cc4e849
kakas <shrimptrain@gmail.com>
2020-04-16 17:05:51 +0800

    split phase 2

--- chapter_6/11_split_phase.js
+++ chapter_6/11_split_phase.js
@@ -1,3 +1,13 @@
-const orderData = orderString.split(/\s+/);
-const productPrice = priceList[orderData[0].split("-")[1]];
-const orderPrice = parseInt(orderData[1]) * productPrice;
+const orderRecord = parseOrder(order);
+const orderPrice = price(orderRecord, priceList);
+
+function parseOrder(aString) {
+  const values =  aString.split(/\s+/);
+  return ({
+    productID: values[0].split("-")[1],
+    quantity: parseInt(values[1]),
+  });
+}
+function price(order, priceList) {
+  return order.quantity * priceList[order.productID];
+}
