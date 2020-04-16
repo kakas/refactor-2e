150ad332ba1a924b46832835ff1a734725fc8cfa
kakas <shrimptrain@gmail.com>
2020-04-16 17:09:56 +0800

    split phase 8

--- chapter_6/11_split_phase.js
+++ chapter_6/11_split_phase.js
@@ -16,14 +16,14 @@ function priceOrder(product, quantity, shippingMethod) {
   const basePrice = product.basePrice * quantity;
   const discount = Math.max(quantity - product.discountThreshold, 0)
     * product.basePrice * product.discountRate;
-  const priceData = {basePrice: basePrice, quantity: quantity};
-  const price =  applyShipping(priceData, shippingMethod, discount);
+  const priceData = {basePrice: basePrice, quantity: quantity, discount: discount};
+  const price =  applyShipping(priceData, shippingMethod);
   return price;
 }
-function applyShipping(priceData, shippingMethod, discount) {
+function applyShipping(priceData, shippingMethod) {
   const shippingPerCase = (priceData.basePrice > shippingMethod.discountThreshold)
     ? shippingMethod.discountedFee : shippingMethod.feePerCase;
   const shippingCost = priceData.quantity * shippingPerCase;
-  const price =  priceData.basePrice - discount + shippingCost;
+  const price =  priceData.basePrice - priceData.discount + shippingCost;
   return price;
 }
