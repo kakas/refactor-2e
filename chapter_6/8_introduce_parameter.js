be0d4a79d240eba67e6a467d610d39006d8ef703
kakas <shrimptrain@gmail.com>
2020-04-16 16:38:32 +0800

    introduce paramter 2

--- chapter_6/8_introduce_parameter.js
+++ chapter_6/8_introduce_parameter.js
@@ -13,3 +13,11 @@ function readingsOutsideRange(station, min, max) {
 }
 
 alerts = readingsOutsideRange(station, operatingPlan.temperatureFloor, operatingPlan.temperatureCeiling);
+
+class NumberRange {
+  constructor(min, max) {
+    this._data = {min: min, max: max};
+  }
+  get min() {return this._data.min;}
+  get max() {return this._data.max;}
+}
