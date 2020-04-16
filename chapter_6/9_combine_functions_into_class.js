f99e62abf4feaa519e9bb21483d43127a69fa994
kakas <shrimptrain@gmail.com>
2020-04-16 16:50:44 +0800

    combine_functions_into_class 6

--- chapter_6/9_combine_functions_into_class.js
+++ chapter_6/9_combine_functions_into_class.js
@@ -17,8 +17,9 @@ class Reading {
 }
 
 // client 1
-const aReading = acquireReading();
-const baseCharge = baseRate(aReading.month, aReading.year) * aReading.quantity;
+const rawReading = acquireReading();
+const aReading = new Reading(rawReading);
+const baseCharge = aReading.baseCharge;
 
 // client 2
 const aReading = acquireReading();
