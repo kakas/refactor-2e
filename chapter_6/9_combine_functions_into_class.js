4246efb5031c4b6589526c2fd9d7adff44938bae
kakas <shrimptrain@gmail.com>
2020-04-16 16:45:47 +0800

    combine_functions_into_class 2

--- chapter_6/9_combine_functions_into_class.js
+++ chapter_6/9_combine_functions_into_class.js
@@ -1,5 +1,18 @@
 // reading = {customer: "ivan", quantity: 10, month: 5, year: 2017};
 
+class Reading {
+  constructor(data) {
+    this._customer = data.customer;
+    this._quantity = data.quantity;
+    this._month = data.month;
+    this._year = data.year;
+  }
+  get customer() {return this._customer;}
+  get quantity() {return this._quantity;}
+  get month()    {return this._month;}
+  get year()     {return this._year;}
+}
+
 // client 1
 const aReading = acquireReading();
 const baseCharge = baseRate(aReading.month, aReading.year) * aReading.quantity;
