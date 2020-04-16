28219a31d220192451436ff8bdf3daab3ed4a285
kakas <shrimptrain@gmail.com>
2020-04-16 16:30:17 +0800

    encapsulate variable 4

--- chapter_6/6_encapsulate_variable.js
+++ chapter_6/6_encapsulate_variable.js
@@ -3,3 +3,10 @@ export function defaultOwner()       {return defaultOwnerData;}
 export function setDefaultOwner(arg) {defaultOwnerData = arg;}
 spaceship.owner = getDefaultOwner();
 setDefaultOwner({firstName: "Rebecca", lastName: "Parsons"});
+
+// 這樣 OK 嗎？
+const owner1 = defaultOwner();
+assert.equal("Fowler", owner1.lastName, "when set");
+const owner2 = defaultOwner();
+owner2.lastName = "Parsons";
+assert.equal("Parsons", owner1.lastName, "after change owner2"); // is this ok?
