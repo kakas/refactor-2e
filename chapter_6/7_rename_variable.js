6dd59fdc10c79c58892e6dae8ffecda1bb1e2f5b
kakas <shrimptrain@gmail.com>
2020-04-16 16:35:12 +0800

    rename variable 2

--- chapter_6/7_rename_variable.js
+++ chapter_6/7_rename_variable.js
@@ -1,3 +1,7 @@
 let tpHd = "untitled";
-result += `<h1>${tpHd}</h1>`;
-tpHd = obj['articleTitle'];
+
+result += `<h1>${title()}</h1>`;
+setTitle(obj['articleTitle']);
+function title()       {return tpHd;}
+function setTitle(arg) {tpHd = arg;}
+
