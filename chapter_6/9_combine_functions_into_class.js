63a4dac034cb57e352845d9488c3e83cd1d602a7
kakas <shrimptrain@gmail.com>
2020-04-16 16:45:23 +0800

    combine_functions_into_class 1

---
+++ chapter_6/9_combine_functions_into_class.js
@@ -0,0 +1,17 @@
+// reading = {customer: "ivan", quantity: 10, month: 5, year: 2017};
+
+// client 1
+const aReading = acquireReading();
+const baseCharge = baseRate(aReading.month, aReading.year) * aReading.quantity;
+
+// client 2
+const aReading = acquireReading();
+const base = (baseRate(aReading.month, aReading.year) * aReading.quantity);
+const taxableCharge =  Math.max(0, base - taxThreshold(aReading.year));
+
+// client 3
+const aReading = acquireReading();
+const basicChargeAmount = calculateBaseCharge(aReading);
+function calculateBaseCharge(aReading) {
+  return  baseRate(aReading.month, aReading.year) * aReading.quantity;
+}
