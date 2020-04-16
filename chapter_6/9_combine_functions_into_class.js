2e5ba216103e78c41a9a880850a8265639d50a13
kakas <shrimptrain@gmail.com>
2020-04-16 16:54:01 +0800

    combine_functions_into_class 9

--- chapter_6/9_combine_functions_into_class.js
+++ chapter_6/9_combine_functions_into_class.js
@@ -14,6 +14,9 @@ class Reading {
   get baseCharge() {
     return  baseRate(this.month, this.year) * this.quantity;
   }
+  get taxableCharge() {
+    return  Math.max(0, this.baseCharge - taxThreshold(this.year));
+  }
 }
 
 // client 1
@@ -24,10 +27,7 @@ const baseCharge = aReading.baseCharge;
 // client 2
 const rawReading = acquireReading();
 const aReading = new Reading(rawReading);
-const taxableCharge = taxableChargeFn(aReading);
-function taxableChargeFn(aReading) {
-  return Math.max(0, aReading.baseCharge - taxThreshold(aReading.year));
-}
+const taxableCharge = aReading.taxableCharge;
 
 // client 3
 const rawReading = acquireReading();
