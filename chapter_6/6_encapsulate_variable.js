953138d41f573a4ee3769e29cc094bd04478c23a
kakas <shrimptrain@gmail.com>
2020-04-16 16:27:01 +0800

    encapsulate variable 2

--- chapter_6/6_encapsulate_variable.js
+++ chapter_6/6_encapsulate_variable.js
@@ -1,3 +1,5 @@
 let defaultOwner = {firstName: "Martin", lastName: "Fowler"};
-spaceship.owner = defaultOwner;
-defaultOwner = {firstName: "Rebecca", lastName: "Parsons"};
+function getDefaultOwner()    {return defaultOwner;}
+function setDefaultOwner(arg) {defaultOwner = arg;}
+spaceship.owner = getDefaultOwner();
+setDefaultOwner({firstName: "Rebecca", lastName: "Parsons"});
