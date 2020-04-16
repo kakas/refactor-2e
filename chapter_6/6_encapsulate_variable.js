872c8f51018dc503187020d0ba72be361755511c
kakas <shrimptrain@gmail.com>
2020-04-16 16:32:44 +0800

    encapsulate variable 6

--- chapter_6/6_encapsulate_variable.js
+++ chapter_6/6_encapsulate_variable.js
@@ -11,6 +11,16 @@ const owner2 = defaultOwner();
 owner2.lastName = "Parsons";
 assert.equal("Parsons", owner1.lastName, "after change owner2"); // is this ok?
 
+class Person {
+  constructor(data) {
+    this._lastName = data.lastName;
+    this._firstName = data.firstName
+  }
+  get lastName() {return this._lastName;}
+  get firstName() {return this._firstName;}
+  // and so on for other properties
+}
+
 let defaultOwnerData = {firstName: "Martin", lastName: "Fowler"};
-export function defaultOwner()       {return Object.assign({}, defaultOwnerData);}
+export function defaultOwner()       {return new Person(defaultOwnerData);}
 export function setDefaultOwner(arg) {defaultOwnerData = arg;}
