a519a93c49e7b1c4d4ca0d324913045e852b47fd
kakas <shrimptrain@gmail.com>
2020-04-16 16:24:10 +0800

    change function declaration 12

--- chapter_6/5_change_function_declaration.js
+++ chapter_6/5_change_function_declaration.js
@@ -15,8 +15,8 @@ class Book {
   }
 }
 
-function xxNEWinNewEngland(stateCode) {
+function inNewEngland(stateCode) {
   return ["MA", "CT", "ME", "VT", "NH", "RI"].includes(stateCode);
 }
 
-const newEnglanders = someCustomers.filter(c => xxNEWinNewEngland(c.address.state));
+const newEnglanders = someCustomers.filter(c => inNewEngland(c.address.state));
