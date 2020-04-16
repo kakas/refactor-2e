37e1941cdd0e8e7af37ef91798bf39233db48dad
kakas <shrimptrain@gmail.com>
2020-04-16 16:19:28 +0800

    change function declaration 4

--- chapter_6/5_change_function_declaration.js
+++ chapter_6/5_change_function_declaration.js
@@ -7,6 +7,9 @@ function circumference(radius) {
 
 class Book {
   addReservation(customer) {
+    this.zz_addReservation(customer);
+  }
+  zz_addReservation(customer) {
     this._reservations.push(customer);
   }
 }
